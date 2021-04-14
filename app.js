57b4feb863659180eab93b67e0fd865acec98390
chrisrachal <cjrachaljr@gmail.com>
2021-04-14 10:38:09 -0500

    app: add condtional and boolean operations

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+console.log(5+5);
+
+console.log("I don't like anchovies");
+console.log(parseInt('33'));
+console.log(parseFloat('33.3'));
+
+
+
+
+//Assignment Operators
+let age = 27;
+let firstName = 'Chris';
+
+console.log(firstName.replace('s', 'c'));
+
+console.log(5 == 5);
+
+let doesEqual = 5 == 5;
+console.log(doesEqual);
+
+let doorClosed = false;
+
+if (doorClosed) {
+    console.log('Open door');
+}
+   else {
+    console.log('Walk through the door');
+   } 
+
+// Truthy vs falsey
+age = 0;
+
+if (age) {
+    console.log('Age is zero');
+}
+ else {
+     console.log('Age is not zero');
+ }
+
+
+if (firstName) {
+    console.log('This is true');
+}
+ else {
+     console.log('This is false');
+ }
+
+ let stopLight = 'red';
+
+ if (stopLight === 'green') {
+     console.log('GO');
+ } else if (stopLight === 'red') {
+     console.log('STOP');
+ } else {
+     console.log('SLOW DOWN');
+ }
